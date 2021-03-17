a620e77cf9bbfe51dd811cf647968036c50893cb
Erwin Hessing <ejhessing@gmail.com>
2021-03-17 15:47:32 +1300

    Add raygun

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import RaygunClient, { LogLevel} from "raygun4reactnative"
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+const options = {
+  apiKey: '',
+  enableCrashReporting: true,
+  disableNativeCrashReporting: false,
+  disableUnhandledPromiseRejectionReporting: false,
+  enableRealUserMonitoring: true,
+  disableNetworkMonitoring: false,
+  logLevel: LogLevel.info,
+};
+
+
+
 export default function App() {
+  RaygunClient.init(options)
+  throw new Error('foobar')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
